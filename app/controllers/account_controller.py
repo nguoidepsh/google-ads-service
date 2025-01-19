045f0b46469750f5acec187d2366eccf5c117a8f
thangnb <nbthang2212@gmail.com>
2025-01-19 16:37:12 +0700

    create Accounts

--- app/controllers/account_controller.py
+++ app/controllers/account_controller.py
@@ -1,9 +1,12 @@
+from typing import List
 from schemas.account_schema import AccountCreate, AccountUpdate
 from db.db_connector import DB_SESSION
 from fastapi import Depends, Request
 from models.account_model import Account
 from sqlmodel import select
 from starlette.exceptions import HTTPException
+from sqlalchemy.exc import IntegrityError
+
 
 async def get_accounts_by_uuid(session: DB_SESSION, request: Request):
     userinfo = request.state.auth_data
@@ -48,15 +51,59 @@ async def create_account(session: DB_SESSION, request: Request, account_data: Ac
     return account
 
 
+async def create_accounts(
+    session: DB_SESSION,
+    request: Request,
+    accounts_data: List[AccountCreate]
+) -> List[Account]:
+    """
+    Create multiple accounts.
+    """
+    userinfo = request.state.auth_data
+    created_accounts = []
+    failed_accounts = []
+    for account_data in accounts_data:
+        try:
+            existing_account = session.query(Account).filter(
+                Account.customer_id == account_data.customer_id).first()
+            if existing_account:
+                failed_accounts.append({
+                    "account_data": account_data,
+                    "error": f"Customer ID {account_data.customer_id} already exists."
+                })
+                continue
+
+            account = Account(**account_data.model_dump(),
+                              uuid=userinfo["uuid"])
+            session.add(account)
+            created_accounts.append(account)
+
+        except Exception as e:
+            session.rollback()
+            failed_accounts.append({
+                "account_data": account_data,
+                "error": f"Error: {str(e)}"
+            })
+            continue
+    session.commit()
+    for account in created_accounts:
+        session.refresh(account)
+    return {
+        "created_accounts": created_accounts,
+        "failed_accounts": failed_accounts
+    }
+
+
 async def update_account(session: DB_SESSION, updates: AccountUpdate):
     """
     Update an existing account by id.
     """
-    account = session.exec(select(Account).where(Account.id == updates.id)).first()
+    account = session.exec(select(Account).where(
+        Account.id == updates.id)).first()
     if not account:
         raise HTTPException(status_code=404, detail="Account not found")
 
-    update_data = updates.model_dump() 
+    update_data = updates.model_dump()
 
     for key, value in update_data.items():
         setattr(account, key, value)
--- app/routers/account_route.py
+++ app/routers/account_route.py
@@ -1,8 +1,10 @@
+from pprint import pprint
 from typing import List
-from fastapi import APIRouter, Body, Depends
+from fastapi import APIRouter, Body, Depends, Request
 from db.db_connector import DB_SESSION
 from schemas.account_schema import AccountCreate, AccountUpdate
 from controllers.account_controller import (
+    create_accounts,
     get_accounts_by_uuid,
     get_accounts,
     create_account,
@@ -14,11 +16,13 @@ from schemas.base_schema import BaseResponseModel, MultipeResponseModel, SingleR
 
 router = APIRouter()
 
+
 @router.get("")
 async def list_accounts_by_uuid(
     accounts: List[Account] = Depends(get_accounts_by_uuid),
 ) -> MultipeResponseModel:
     return MultipeResponseModel(data=accounts)
+
 
 @router.get("/all")
 async def list_accounts(
@@ -26,17 +30,34 @@ async def list_accounts(
 ) -> MultipeResponseModel:
     return MultipeResponseModel(data=accounts)
 
+
 @router.post("")
 async def create_account_endpoint(
     account: AccountCreate = Depends(create_account)
 ) -> SingleResponseModel[Account]:
     return SingleResponseModel(data=account, message="Account created successfully")
 
+
+@router.post("/bulk")
+async def create_accounts_endpoint(
+    accounts_data: List[AccountCreate] = Depends(create_accounts)
+) -> MultipeResponseModel:
+    """
+    Create multiple accounts.
+    """
+    created_accounts = accounts_data.get("created_accounts", [])
+    failed_accounts = accounts_data.get("failed_accounts", [])
+
+    pprint({"created": created_accounts, "failed": failed_accounts}, indent=2)
+    return MultipeResponseModel(data=[{"created": created_accounts}, {"failed": failed_accounts}])
+
+
 @router.put("")
 async def update_account_endpoint(
     account: AccountUpdate = Depends(update_account)
-) -> SingleResponseModel[Account]:    
+) -> SingleResponseModel[Account]:
     return SingleResponseModel(data=account, message="Account updated successfully")
+
 
 @router.delete("/{id}")
 async def delete_account_endpoint(
